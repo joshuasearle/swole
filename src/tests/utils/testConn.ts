a36b8d9432ac41c269a173cc2333f30d9640d54f
Josh Searle <joshryansearle@gmail.com>
2022-01-02 01:10:51 +1100

    Make testing database vars constant

--- src/tests/utils/testConn.ts
+++ src/tests/utils/testConn.ts
@@ -5,11 +5,11 @@ dotenv.config()
 
 const testTypeormConfig: ConnectionOptions = {
   type: "postgres",
-  host: process.env["DB_HOST"],
-  port: +process.env["DB_PORT"]!,
-  username: process.env["DB_USERNAME"],
-  password: process.env["DB_PASSWORD"],
-  database: process.env["DB_TEST_NAME"],
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "swole_testing",
   synchronize: true,
   entities: [__dirname + "/../../**/*.entity.ts"],
 }
