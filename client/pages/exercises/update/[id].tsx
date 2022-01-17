435f1ddb14e17afa5297e8c49a72951c62ac63d5
Josh Searle <joshryansearle@gmail.com>
2022-01-18 03:30:41 +1100

    Rename exercise start

--- client/pages/exercises/update/[id].tsx
+++ client/pages/exercises/update/[id].tsx
@@ -1,17 +1,34 @@
 import type { NextPage } from "next"
 import { observer } from "mobx-react"
 import { useRouter } from "next/router"
-import { useMemo } from "react"
+import { useEffect, useState } from "react"
 import { useStore } from "../../../store/store"
+import error from "../../../error/error"
+import { ExerciseFragment } from "../../../generated/graphql"
 
 const CreateExercise: NextPage = observer((props) => {
   const router = useRouter()
   const store = useStore()
+  const [exercise, setExercise] = useState<null | ExerciseFragment>(null)
 
-  const { id } = router.query
-  const exercise = useMemo(() => {
-    return store.getExerciseById(id as string)
-  }, [id])
+  useEffect(() => {
+    const { id } = router.query
+    if (!id) {
+      error(router)
+      return
+    }
+
+    const exercise = store.getExerciseById(id as string)
+
+    if (!exercise) {
+      error(router)
+      return
+    }
+
+    setExercise(exercise)
+  }, [])
+
+  if (!exercise) return <div></div>
 
   return <div>{JSON.stringify(exercise)}</div>
 })
